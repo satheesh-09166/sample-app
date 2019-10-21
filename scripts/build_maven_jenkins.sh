77b52cbf3abdcf91ff6ed22e32ec46fceaedbee5
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-10-21 12:26:58 +0530

    Update build_maven_jenkins.sh

--- scripts/build_maven_jenkins.sh
+++ scripts/build_maven_jenkins.sh
@@ -1,3 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 
-${MAVEN_BIN}/mvn clean package
+# Additional flags for build script
+additional_flags=$@;
+
+# Check if we are in Jenkins or not if we are, then just keep going
+if [ -z "${BUILD_NUMBER}" ]; then
+  cd /workspace
+fi
+
+mvn -U clean install 
