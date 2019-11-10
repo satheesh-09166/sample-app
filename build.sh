b5be409ea09ce6221537110c912ce324f1c632b9
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:37:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ docker login -u devopsmptech -p admin@987
 docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
 mkdir -p /var/lib/jenkins/target  || true
+docker run -v $(pwd):/maven -v /var/lib/jenkins/target:/maven/target devopsmptech/mavenimage:1 mvn clean package
 cp /var/lib/jenkins/target/*.war /var/lib/jenkins/builds/
 mv /var/lib/jenkins/builds/*.war /var/lib/jenkins/builds/myapp.war
 docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
