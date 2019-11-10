017f4a375e2becad4db2ec7196420735aff02746
suresh <suresh@gmail.com>
2019-11-10 12:58:16 +0530

    message

--- welcome.sh
+++ welcome.sh
@@ -1,11 +1,1 @@
-#! /bin/#!/usr/bin/env bash
-
-
-yum install httpd -y
-chkconfig httpd on
-service httpd start
-this is dummy line
-echo "welcome to apache server"
-first dummy line
-this is seond dummy line
 3rd dummy line
