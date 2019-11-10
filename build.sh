9aef36ef3a332e592693ef0215a7be0040801a65
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:31:33 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+version=$(cat pom.xml | grep "</version>" | head -1 | awk '{print $1}' | sed "s/<version>//" | sed "s/<.*//")
 docker login -u devopsmptech -p admin@987
 docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
@@ -10,3 +11,5 @@ rm -rf /var/lib/jenkins/builds
 mkdir -p /var/lib/jenkins/builds || true
 cp /var/lib/jenkins/target/*.war /var/lib/jenkins/builds/
 mv /var/lib/jenkins/builds/*.war /var/lib/jenkins/builds/myapp.war
+docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
+docker push devopsmptech/sample-application:$version
