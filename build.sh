fd8c7067e5ead662bcc88a509d6f9ec36303f659
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:43:28 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@ version=$(cat pom.xml | grep "</version>" | head -1 | awk '{print $1}' | sed "s/
 docker login -u devopsmptech -p admin@987
 docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
-mkdir -p /var/lib/jenkins/target  || true
-docker run -v $(pwd):/maven -v /var/lib/jenkins/target:/maven/target devopsmptech/mavenimage:1 mvn package
-cp /var/lib/jenkins/target/*.war /var/lib/jenkins/builds/
-mv /var/lib/jenkins/builds/*.war /var/lib/jenkins/builds/myapp.war
+docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn package
+mv /var/lib/jenkins/workspace/Docker-FULL-FLOW/target/*.war /var/lib/jenkins/workspace/Docker-FULL-FLOW/target/myapp.war
 sleep 10
 docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
 docker push devopsmptech/sample-application:$version
