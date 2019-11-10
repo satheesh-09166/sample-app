d9f59c8236a8e987cd0589c7101a05edb28a6fc9
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 11:52:32 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+docker login -u devopsmptech -p admin@987
+docker build -t devopsmptech/mavenimage:1 .
+docker push devopsmptech/mavenimage:1
+docker run -it -v $(pwd):/maven devopsmptech/mavenimage:1 mvn clean package
