b33fcb77de3b9bd55b51f5a2f9a84f4a7082e618
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:49:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ version=$(cat pom.xml | grep "</version>" | head -1 | awk '{print $1}' | sed "s/
 docker login -u devopsmptech -p admin@987
 docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
-docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn package
+docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn clean package
 pwd
 ls target/
 #cd /target
