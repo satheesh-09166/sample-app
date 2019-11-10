b7ae324a163c4d35c65d376fa4457f22df2be5d9
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:47:12 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,10 @@ docker login -u devopsmptech -p admin@987
 docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
 docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn package
-cd /target
-mv *.war myapp.war
-sleep 10
-docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
-docker push devopsmptech/sample-application:$version
+pwd
+ls
+#cd /target
+#mv *.war myapp.war
+#sleep 10
+#docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
+#docker push devopsmptech/sample-application:$version
