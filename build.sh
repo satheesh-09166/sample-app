f60caf77e4e85f714a00a122133799ca88094d60
suresh <suresh@gmail.com>
2019-11-10 13:08:36 +0530

    message

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 version=$(cat pom.xml | grep "</version>" | head -1 | awk '{print $1}' | sed "s/<version>//" | sed "s/<.*//")
 docker login -u devopsmptech -p admin@987
-docker build -t devopsmptech/mavenimage:1 .
-docker push devopsmptech/mavenimage:1
+docker build -t docker.io/devopsmptech/mavenimage:1 .
+docker push docker.io/devopsmptech/mavenimage:1
 docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn clean package
 pwd
 sleep 10
-docker build -f Dockerfiletomcat -t devopsmptech/sample-application:$version .
+docker build -f Dockerfiletomcat -t docker.io/devopsmptech/sample-application:$version .
 docker login -u devopsmptech -p admin@987
-docker push devopsmptech/sample-application:$version
+docker push docker.io/devopsmptech/sample-application:$version
 docker rm -f $(docker ps -aq)
 docker rmi -f $(docker images -q)
