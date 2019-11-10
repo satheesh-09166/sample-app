f73930a40f008e27e0e743311f4aedf72271418c
suresh <suresh@gmail.com>
2019-11-10 13:20:21 +0530

    message

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@ docker push devopsmptech/mavenimage:1
 docker run -v $(pwd):/maven devopsmptech/mavenimage:1 mvn clean package
 pwd
 sleep 10
-docker build -f Dockerfiletomcat -t sample-application:9 .
-docker tag sample-application:9 devopsmptech/sample-application:9
+version=$(cat pom.xml | grep "</version>" | head -1 | awk '{print $1}' | sed "s/<version>//" | sed "s/<.*//")
+docker build -f Dockerfiletomcat -t sample-application:$version .
+docker tag sample-application:$version devopsmptech/sample-application:$version
 docker login docker.io -u devopsmptech -p admin@987
-docker push devopsmptech/sample-application:9
+docker push devopsmptech/sample-application:$version
 docker rm -f $(docker ps -aq)
 docker rmi -f $(docker images -q)
