9e54ddd2d3d236726c1ed45bcb13fd5e591b64c4
Suresh Babu <41568630+suresh-devops@users.noreply.github.com>
2019-11-10 12:04:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ docker build -t devopsmptech/mavenimage:1 .
 docker push devopsmptech/mavenimage:1
 mkdir -p /var/lib/jenkins/target  || true
 docker run -v $(pwd):/maven -v /var/lib/jenkins/target:/maven/target devopsmptech/mavenimage:1 mvn package
-
+mkdir -p /var/lib/jenkins/builds || true
+cp /var/lib/jenkins/target/*.war /var/lib/jenkins/builds/
